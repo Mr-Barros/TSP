4ca7dcc443668f56d7471f27d36746af91827310
Davi Martignoni Barros <davimartignonibarros@gmail.com>
2026-05-21 09:17:39 -0300

    feat: add implementation for SimulatedAnnealingSolver::solve. Includes helper function SimulatedAnnealingSolver::acceptWithProbability.

--- src/algorithm/SimulatedAnnealingSolver.cpp
+++ src/algorithm/SimulatedAnnealingSolver.cpp
@@ -1,6 +1,42 @@
 #include "SimulatedAnnealingSolver.h"
 
+#include "util/Random.h"
+
 bool SimulatedAnnealingSolver::solve()
 {
-    return false;
+    Solution currentSolution = solution;
+
+    currentSolution.evaluate();
+
+    double temperature = 10000;
+    double decayRate = 0.995;
+
+    while (temperature > 1) {
+        Solution candidateSolution = currentSolution;
+        perturbation->randomMove(candidateSolution);
+        candidateSolution.evaluate();
+
+        if (acceptWithProbability(currentSolution.cost, candidateSolution.cost, temperature))
+        {
+            currentSolution = candidateSolution;
+            neighborhood->localSearch(currentSolution);
+            currentSolution.evaluate();
+
+            if (currentSolution.cost < solution.cost)
+                solution = currentSolution;
+        }
+
+        temperature *= decayRate;
+    }
+
+    return true;
+}
+
+bool SimulatedAnnealingSolver::acceptWithProbability(double currentCost, double candidateCost, double temperature)
+{
+    double probability = exp(-(candidateCost - currentCost) / temperature);
+    if (probability > 1)
+        return true;
+
+    return Random::randomDouble() < probability;
 }
--- src/algorithm/SimulatedAnnealingSolver.h
+++ src/algorithm/SimulatedAnnealingSolver.h
@@ -1,9 +1,17 @@
 #pragma once
 
 #include "AbstractSolver.h"
+#include "neighborhoods/Neighborhood.h"
+#include "neighborhoods/Shift.h"
 
 class SimulatedAnnealingSolver : public AbstractSolver {
 public:
 
     bool solve() override;
+
+private:
+    Neighborhood* neighborhood = new Shift();
+    Neighborhood* perturbation = new Shift();
+
+    bool acceptWithProbability(double currentCost, double candidateCost, double temperature);
 };
